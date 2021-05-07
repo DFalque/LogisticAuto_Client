f317651f4b71b2a9e26c4d6f2e514ce9eff09964
DFalque <falque@me.com>
2021-05-07 12:13:13 +0200

    firebase 2

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+//COMPONENTS
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,87 +18,45 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+// NAVIGATOR
+import {createStackNavigator} from '@react-navigation/stack';
+//import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+// SCREENS LOGIN
+import WelcomeScreen from './src/screens/login/WelcomeScreen';
+import Login from './src/screens/login//Login';
+import SignUp from './src/screens/login/SignUp';
+// SCREENS MAIN
+import Home from './src/screens/main/Home';
+
+//CREATE LOGIN STACK
+const LoginStack = createStackNavigator();
+
+const LoginStackScreen = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <LoginStack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{headerShown: false}}
+      tabBarOption={{}}>
+      <LoginStack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        Options={{}}
+      />
+      <LoginStack.Screen name="SignUp" component={SignUp} Options={{}} />
+      <LoginStack.Screen name="Login" component={Login} Options={{}} />
+    </LoginStack.Navigator>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+export default function App() {
+  const [user, setUser] = useState();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <LoginStackScreen />
+    </NavigationContainer>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({});
---
+++ database/Firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyCSyq8xcMN5O3HFlBuVwllxPW-uxNcbky4',
+  authDomain: 'statuscar-9cd8a.firebaseapp.com',
+  projectId: 'statuscar-9cd8a',
+  storageBucket: 'statuscar-9cd8a.appspot.com',
+  messagingSenderId: '622947941775',
+  appId: '1:622947941775:web:12a6f4735e25cc15a31ffa',
+  measurementId: 'G-0E9MBE9VYS',
+};
